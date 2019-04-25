5e5bb338fcdc0d4885e69ae7089e06607e7ec32b
Sudoxe <lintonf@pm.me>
2019-04-25 10:52:51 +0100

    Add font and character replacement customisation options

--- main.py
+++ main.py
@@ -4,17 +4,37 @@ import time
 
 from pyfiglet import figlet_format
 
+# VARIABLES
+
+# Padding
+top_padding_amount      = 0
+right_padding_amount    = 0
+bottom_padding_amount   = 0
+left_padding_amount     = 0
+
+# Font
+font_name = '3x5' # (for list of fonts see: http://www.figlet.org/fontdb.cgi)
+custom_char_replace = {'#': '\u2588'} # Specify characters to replace
+
+# Time (for formatting options see: http://strftime.org/)
+time_format = '%H:%M:%S'
+
 while True:
+    # Clear the screen to make the clock appear 'in-place'
     os.system('clear')
 
-    h = str(datetime.datetime.now().strftime('%H'))
-    m = str(datetime.datetime.now().strftime('%M'))
-    s = str(datetime.datetime.now().strftime('%S'))
+    # Convert the current time to the defined format
+    time_string = datetime.datetime.now().strftime(time_format)
+ 
+    # Turn the string into a figlet ASCII art string
+    figlet_string = figlet_format(time_string, font=font_name)
 
-    time_string = '%s:%s:%s' % (h, m, s)
-    figlet = figlet_format(time_string, font='3x5')
-    replace_with_char = figlet.replace('#', "\u2588")
+    # Replace any characters
+    for char in custom_char_replace:
+        figlet_string = figlet_string.replace(char, custom_char_replace[char])
 
-    print(replace_with_char)
+    # Print the string
+    print(figlet_string)
 
+    # Sleep
     time.sleep(1)
